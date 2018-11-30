4d54204b4ae14094accb0a19502817f1289da8d4
rasselict07 <rezaul07012>
2018-11-30 19:13:01 +0600

    documetaion on area and java doc for codesignal package

--- src/codesignal/ShapeArea.java
+++ src/codesignal/ShapeArea.java
@@ -1,21 +1,30 @@
 package codesignal;
 
+/**
+ * Calculate the area of n value using recursion
+ */
 public class ShapeArea {
     public static void main(String[] args) {
-        System.out.println(new ShapeArea().shapeArea(4));
+        ShapeArea shapeArea = new ShapeArea();
+        int result = shapeArea.shapeArea(5);
+        System.out.println("Get result " + result);
     }
 
-    // Using recursion to use the previous shape's area plus a multiple of 4 to find the new shape's area
+    /**
+     * Using recursion to use the previous shape's area plus a multiple of 4 to find the new shape's area
+     *
+     * @param n
+     * @return int of shape area
+     */
     int shapeArea(int n) {
-        System.out.println("methods");
-        if(n == 1) {
+        System.out.println("initialize methods");
+        if (n == 1) {
             int int_area = 1;
-            System.out.println("methods "+int_area);
+            System.out.println("methods " + int_area);
             return int_area;
-        }
-        else {
-            int int_area = shapeArea(n-1) + (4 * (n-1));
-            System.out.println("methods shapeArea("+(n-1)+")");
+        } else {
+            int int_area = shapeArea(n - 1) + (4 * (n - 1));
+            System.out.println("methods shapeArea(" + (n - 1) + ")");
             return int_area;
         }
 
